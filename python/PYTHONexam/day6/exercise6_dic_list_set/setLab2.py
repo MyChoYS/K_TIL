35064b25812ff472f3d6bd945d9b1697a04d6674
Yong Sang Cho <sin94soon@gmail.com>
2021-01-15 17:54:01 +0900

    css exercise

--- python/PYTHONexam/day6/exercise6_dic_list_set/setLab2.py
+++ python/PYTHONexam/day6/exercise6_dic_list_set/setLab2.py
@@ -1,9 +1,14 @@
 import random
 num = set()
+count=0
+for i in range(946):
+    num = set()
+    while len(num) < 6:
+        lotto = random.randint(1, 45)
+        num.add(lotto)
+        # num.add(random.randint(1,45)하나로 퉁칠수도있다.
+    count +=1
+    print(count,num)
 
-while len(num) <6:
-    lotto = random.randint(1, 45)
-    num.add(lotto)
-    #num.add(random.randint(1,45)하나로 퉁칠수도있다.
-print("행운의 로또번호 :",num)
+#print("행운의 로또번호 :",num)
 
